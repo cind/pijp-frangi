a6da4c92830e5e6c9251cfd07a9150a88d074697
Serena Tang <vhasfctangs1@japan.vacind.org>
2024-02-21 23:13:30 -0800

    testing other analysis script

--- pijp_frangi/other_analysis.py
+++ pijp_frangi/other_analysis.py
@@ -1,3 +1,13 @@
+import ipdb
+
+import os
+import subprocess
+import logging
+import argparse
+import textwrap
+import string
+import datetime
+
 import numpy as np
 import shutil
 import pandas as pd
@@ -6,6 +16,37 @@ import gzip
 import nibabel as nib
 from matplotlib import pyplot as plt
 from scipy import stats
+import glob
+
+# Before loading NUMPY: HACK to avoid seg fault on some hosts.
+os.environ["OMP_NUM_THREADS"] = "1"
+
+import repo
+from pijp import util, coding
+from pijp.repositories import ProcessingLog
+from pijp.core import Step, get_project_dir
+from pijp.engine import run_module, run_file
+from pijp.exceptions import ProcessingError, NoLogProcessingError
+
+LOGGER = logging.getLogger(__name__)
+PROCESS_TITLE = 'pijp-frangi'
+def get_process_dir(project):
+    return os.path.join(get_project_dir(project), PROCESS_TITLE)
+
+def get_case_dir(project, researchgroup, code):
+    cdir = os.path.join(get_process_dir(project), researchgroup, code)
+    if not os.path.exists(cdir):
+        os.makedirs(cdir)
+
+    return cdir
+
+# obtain WMH volume and WMH volume corrected
+# generate average maps
+
+# obtain BG, HIPP segmentations
+# obtain WM, BG, HIPP volumes
+
+# ASL measures
 
 
 
